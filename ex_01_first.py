f34e437513a4f0a20c350adbae36796d2bd14968
albert <albert.greinoecker@gmail.com>
2026-03-25 09:32:41 +0100

    special option for thingworx necessary: client.ws_set_options(path="/Thingworx/WS")

--- ex_01_first.py
+++ ex_01_first.py
@@ -1,3 +1,7 @@
+'''
+How to retrieve and set properties in ThingWorx using Python
+'''
+
 import requests
 from dotenv import load_dotenv
 import os
@@ -7,21 +11,32 @@ load_dotenv()
 app_key = os.getenv("APP_KEY")
 thingworx_url = os.getenv("THINGWORX_URL")
 
-print(app_key)
-print(thingworx_url)
-
 # Header für Authentifizierung und Format
 headers = {
     "AppKey": app_key,
     "Accept": "application/json"
 }
 
-# HTTP-GET an ThingWorx
-response = requests.get(thingworx_url, headers=headers)
+def get_property(name):
+    url = f"{thingworx_url}Things/test_sensor/Properties/{name}"
+    print(url)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("rows", [{}])[0].get("temperature", "Keine Daten")
+    else:
+        print("Fehler beim Abrufen der Daten:", response.status_code, response.text)
+        return None
 
-# Antwort prüfen
-if response.status_code == 200:
-    data = response.json()
-    print("Aktuelle Temperatur:", data.get("Temperature"))
-else:
-    print("Fehler beim Abrufen der Daten:", response.status_code, response.text)
+def set_property(name, value):
+    url = f"{thingworx_url}Things/test_sensor/Properties/{name}"
+    response = requests.put(url, headers=headers, json={name: value})
+    if response.status_code == 200:
+        print("Property updated successfully!")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+
+if __name__ == '__main__':
+    val = get_property("temperature")
+    print(val)
+    set_property("temperature", 25)
